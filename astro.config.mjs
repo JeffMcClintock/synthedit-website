118a494c7cf2d0856100b40dd8e3518ecacf2e98
Jeff McClintock <jef@synthedit.com>
2026-04-27 19:25:08 +1200

    ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/new/',
+	base: '/new-docs/',
 	integrations: [
 		starlight({
 			title: 'SynthEdit',
